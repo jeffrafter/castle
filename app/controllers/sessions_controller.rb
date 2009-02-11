48b9e11cad31da75926544cbb31e5e6c63edc7f7
Jeff Rafter <jeff@baobabhealth.org>
2009-02-11 12:15:33 -0800

    Update readme

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,12 @@ class SessionsController < ApplicationController
   # Handle deactivations
   def verify
     @user = User.find_by_number!(params[:number])
-    sign_user_in(@user)                  
     unless (@user.number_confirmed?)
       @message = Message.create(:number => @user.number, :message => 'Thanks for joining. To get help, reply to this message with "help"')
       @user.number_confirmed = true
       @user.save!
-      render :text => 'halt', :status => :error
-      return
     end
+    sign_user_in(@user)                  
     respond_to do |format|
       format.xml  { render :xml => @user }
     end  
