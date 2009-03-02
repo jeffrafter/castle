b9ada5ff5ee7499bfbe43295615f694670be9599
Jeff Rafter <jeff@baobabhealth.org>
2009-03-02 14:13:26 -0800

    More tweaks for delivery

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,7 +6,7 @@ class Subscription < ActiveRecord::Base
   # We need a scope that allows us to quickly retrieve all subscriptions which
   # need more deliveries today (sorry I didn't wrap these lines)
   named_scope :needy, lambda {|channel_id| {
-    :select => "subscriptions.user_id AS user_id, subscriptions.channel_id AS channel_id, subscriptions.number_per_day AS number_per_day, IFNULL(deliveries.delivery_count, 0) AS delivery_count",
+    :select => "subscriptions.id AS id, subscriptions.user_id AS user_id, subscriptions.channel_id AS channel_id, subscriptions.number_per_day AS number_per_day, IFNULL(deliveries.delivery_count, 0) AS delivery_count",
     :joins => "LEFT OUTER JOIN (SELECT *, COUNT(*) AS delivery_count FROM deliveries) AS deliveries ON deliveries.user_id = subscriptions.user_id AND deliveries.channel_id = subscriptions.channel_id AND deliveries.created_at > '#{Time.today.iso8601}'",
     :group => "subscriptions.user_id, subscriptions.channel_id HAVING IFNULL(deliveries.delivery_count, 0) < subscriptions.number_per_day"  
   }}
