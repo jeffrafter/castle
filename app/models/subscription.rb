d0c569a770706bd863cb09d6b1485041bc5371b5
Jeff Rafter <jeff@baobabhealth.org>
2009-03-02 14:41:00 -0800

    Trying my make the scubscriptions check better

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,9 +6,9 @@ class Subscription < ActiveRecord::Base
   # We need a scope that allows us to quickly retrieve all subscriptions which
   # need more deliveries today (sorry I didn't wrap these lines)
   named_scope :needy, lambda {|channel_id| {
-    :select => "subscriptions.id AS id, subscriptions.user_id AS user_id, subscriptions.channel_id AS channel_id, subscriptions.number_per_day AS number_per_day, IFNULL(deliveries.delivery_count, 0) AS delivery_count",
-    :joins => "LEFT OUTER JOIN (SELECT *, COUNT(*) AS delivery_count FROM deliveries) AS deliveries ON deliveries.user_id = subscriptions.user_id AND deliveries.channel_id = subscriptions.channel_id AND deliveries.created_at > '#{Time.today.iso8601}'",
-    :group => "subscriptions.user_id, subscriptions.channel_id HAVING IFNULL(deliveries.delivery_count, 0) < subscriptions.number_per_day"  
+    :select => "subscriptions.*, IFNULL(deliveries.delivery_count, 0) AS delivery_count",
+    :joins => "LEFT OUTER JOIN (SELECT COUNT(*) AS delivery_count, channel_id, user_id, created_at FROM deliveries GROUP BY channel_id, user_id HAVING created_at > '#{Time.today.iso8601}') AS deliveries ON deliveries.user_id = subscriptions.user_id AND deliveries.channel_id = subscriptions.channel_id",
+    :conditions => "IFNULL(deliveries.delivery_count, 0) < subscriptions.number_per_day"  
   }}
 
   def more
