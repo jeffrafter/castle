d1017cfdb25814d51c6c5d5bc08bdcd70b7b179d
Jeff Rafter <jeff@baobabhealth.org>
2009-03-04 10:14:09 -0800

    Check gateway authenticity

--- lib/forgery_protection_extension.rb
+++ lib/forgery_protection_extension.rb
@@ -1,7 +1,6 @@
 ActionController::RequestForgeryProtection.module_eval do
   alias :original_verify_authenticity_token :verify_authenticity_token
 
-
   # Need to override the default crsf protection. If we have an incoming
   # request then we need an authorized user token to complete the request,
   # if it is a web request handle it in the default manner
@@ -9,9 +8,8 @@ ActionController::RequestForgeryProtection.module_eval do
     if params[:api_key] && params[:format] == 'xml'      
       key = params[:api_key]
       conditions = ['api_key = ? AND (api_key_expires_at < ? OR api_key_expires_at IS NULL)', key, Time.now]
-      user = User.first(:conditions => conditions)                 
-      sign_in_user(user) if user
-      raise(ActionController::InvalidAuthenticityToken) unless user
+      gateway = Gateway.first(:conditions => conditions)                 
+      raise(ActionController::InvalidAuthenticityToken) unless gateway
     else
       original_verify_authenticity_token(*args)       
     end 
