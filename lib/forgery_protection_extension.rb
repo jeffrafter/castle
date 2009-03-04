424487a1be6db42af13af8324370db3a96eca8eb
Jeff Rafter <jeff@baobabhealth.org>
2009-03-04 10:06:58 -0800

    Reactivating authenticity tokens

--- lib/forgery_protection_extension.rb
+++ lib/forgery_protection_extension.rb
@@ -6,11 +6,11 @@ ActionController::RequestForgeryProtection.module_eval do
   # request then we need an authorized user token to complete the request,
   # if it is a web request handle it in the default manner
   def verify_authenticity_token(*args)    
-    if controller.params[:api_key] && controller.params[:format] == 'xml'      
-      key = controller.params[:api_key]
+    if params[:api_key] && params[:format] == 'xml'      
+      key = params[:api_key]
       conditions = ['api_key = ? AND (api_key_expires_at < ? OR api_key_expires_at IS NULL)', key, Time.now]
       user = User.first(:conditions => conditions)                 
-      controller.sign_in_user(user) if user
+      sign_in_user(user) if user
       raise(ActionController::InvalidAuthenticityToken) unless user
     else
       original_verify_authenticity_token(*args)       
