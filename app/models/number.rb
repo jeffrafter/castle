1da3a5a14bf6077a690d1df8d0949a83df8e1d7b
Jeff Rafter <jeff@baobabhealth.org>
2009-03-30 13:20:34 -0700

    Add a couple admin numbers from the states

--- app/models/number.rb
+++ app/models/number.rb
@@ -12,6 +12,7 @@ private
   end
   
   def self.format_number(number)
+    return number if (number == "+19519020972" || number == "+16507991415" || number == "+16506992355")
     raise InvalidPhoneNumberError.new("Invalid number format #{number}") if number == "500" || number == "+5690" || number == "911" || number == "1121611611" || number == "Movistar"
     re = /\+#{country_code}\d{9}/
     number = clean_number(number)      
