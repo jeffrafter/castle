2caf31522f2155586099d9e2b37a4352ab884b00
Jeff Rafter <jeff@baobabhealth.org>
2009-04-01 12:07:31 -0700

    This should be good

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -4,11 +4,11 @@ class LogController < ApplicationController
   rescue_from Exception do |e| render :text => e.message, :status => :error; end
   
   def create
-    kind = (params[:kind] || 'default')
+    kind = (params[:log][:kind] || 'default')
     filename = File.join(RAILS_ROOT, 'log', @gateway.api_key, kind, Time.now.strftime("%Y%m%dT%H:%M:%S.log"))
     FileUtils.mkpath(File.dirname(filename))
     File.open(filename, 'w') do |file|
-      file.write(params[:text])
+      file.write(params[:log][:text])
     end
     render :nothing => true, :status => :ok      
   end    
