961ee40dc75de8b28e19dc677528c982da70dcdd
Jeff Rafter <jeff@baobabhealth.org>
2009-04-03 09:29:57 -0700

    Default locale

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,12 @@
 class EntriesController < ApplicationController
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || 'en'
+  end 
+   
   resource_controller  
+
   def index
     if params[:feed_id]
       @entries = Entry.paginate_by_feed_id(params[:feed_id], :page => params[:page], :per_page => 10, :order => 'created_at DESC')
