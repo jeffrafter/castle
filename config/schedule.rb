8af6c9037c7b191c047aac480d187526463d7836
Jeff Rafter <jeff@baobabhealth.org>
2009-09-13 22:57:18 -0700

    Deliver things more slowly, but more reliably

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,9 @@
-#set :cron_log, "log/cron_log.log"
+set :cron_log, "log/cron_log.log"
 
-#every 1.minutes do
-#  rake 'feeds:deliver'
-#end  
+every 6.minutes do
+  rake 'feeds:deliver'
+end  
 
-#every 6.minutes do
-#  rake 'feeds:fetch'
-#end  
-
-#every 1.day do
-#  rake 'feeds:popular'
-#end
+every 6.minutes do
+  rake 'feeds:fetch'
+end  
