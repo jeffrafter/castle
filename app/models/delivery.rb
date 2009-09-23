d6ae5a1a71c0e6c4d4fe049144fa5f9508f4b00a
Jeff Rafter <jeff@baobabhealth.org>
2009-09-23 12:10:28 -0700

    Beware of users with no gateway when running popular messages with more error handling

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -26,7 +26,7 @@ class Delivery < ActiveRecord::Base
   # Only deliver system messages that have no published date, or that are now
   # published and that have not been delivered already.
   def self.deliver_system_messages_to(user, channel)
-    return unless user.gateway.region_id == channel.region_id
+    return unless user.gateway && user.gateway.region_id && user.gateway.region_id == channel.region_id
     return unless user.active? && user.number_confirmed? && channel.active?
     return if user.quiet_hours? && !channel.emergency?
     priority = channel.emergency? ? PRIORITY[:emergency] : PRIORITY[:normal]
@@ -39,20 +39,20 @@ class Delivery < ActiveRecord::Base
   
   # Find the oldest popular message needed for this user
   def self.deliver_popular_messages_to(user)
-    return unless user.active? && user.number_confirmed?
+    return unless user.active? && user.number_confirmed? && user.gateway && user.gateway.region_id
     return if user.quiet_hours?
     # Should popular messages wait?
     # last_delivery_time = self.last_delivered_entry_time(subscription.user_id)
     # return if last_delivery_time && (Time.zone.now - last_delivery_time) < user.delay.minutes
-    region_id = user.gateway.region_id rescue nil
-    return unless region_id
     popular = Popular.first(:include => :entry,
       :joins => "LEFT JOIN deliveries ON deliveries.entry_id = popular.entry_id AND deliveries.user_id = #{user.id} " +
-               "INNER JOIN channels ON channels.id = popular.channel_id AND channels.region_id = #{region_id}", 
+               "INNER JOIN channels ON channels.id = popular.channel_id AND channels.region_id = #{user.gateway.region_id}", 
       :conditions => 'deliveries.entry_id IS NULL', 
       :limit => 1)
     return unless popular    
     self.deliver(user.id, popular.channel_id, popular.entry, PRIORITY[:normal])    
+  rescue Exception => e
+    Rails.logger.error "Could not deliver popular messages to user #{user.id}: #{e.message}"
   end
   
 private
